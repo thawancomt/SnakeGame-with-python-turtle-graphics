8d7f4b7d191f7d247781a00963eeae7adf7e4409
Thawan Barroso <thawancomt@gmail.com>
2024-04-26 23:08:51 +0100

    Added high score
    Were added the high score feature that change the score color when the player pass the previous highest score

--- SnakeGame.py
+++ SnakeGame.py
@@ -36,6 +36,7 @@ class Score(turtle.Turtle):
         self.color('white')
         
         self.score = 0
+        self.high_score = self.get_high_score()
         
         self.write_score()
     
@@ -44,10 +45,35 @@ class Score(turtle.Turtle):
 
     def update_score(self):
         self.clear()
+        self.check_score_greater_than_high_score()
         self.write_score()
 
     def increase_score(self):
         self.score += 1
+        
+    def check_score_greater_than_high_score(self):
+        if self.score > self.high_score:
+            self.color('red')
+            return True
+            
+    def save_high_score(self):
+        if self.check_score_greater_than_high_score():
+            self.high_score = self.score
+            with open('high_score.txt', mode='w') as file:
+                file.write(str(self.high_score))
+    
+    def get_high_score(self):
+        try:
+            with open('high_score.txt', mode='r') as file:
+                self.high_score = int(file.read())
+                
+        except FileNotFoundError:
+            return 0
+        return self.high_score
+
+
+
+
 
 
 class Game_Engine():
@@ -183,6 +209,8 @@ class Game_Engine():
         
         self.screen.bgcolor('black')
         self.screen.title('Game Over')
+        
+        self.score.save_high_score()
         
         
     def check_collision_with_food(self):
